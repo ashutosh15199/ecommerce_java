e52b2ec8cc309c69efa7b0431de21a23bd044197
Ashutosh_tiwari6554843 <ashutoshkumartiwari686@gail.com>
2025-06-05 01:00:24 +0530

    update pom make application-test

--- src/test/java/com/example/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/example/ecommerce/EcommerceApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // This tells Spring Boot to use application-test.properties
 class EcommerceApplicationTests {
 
 	@Test
